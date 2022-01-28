5b3d25051eb0cd6b66803685e2b97ea538535c1a
Maksim Burtsev <zadrot-lol@list.ru>
2022-01-28 13:38:52 +0300

    V1.2

---
+++ main.py
@@ -0,0 +1,45 @@
+import telebot
+from telebot import types
+import time
+import requests
+from parse_and_delete_f import main, is_new_post_piblished,delete_videos
+
+TOKEN = '5291208463:AAEokJAK6ISX7TiwJ4pBYZKoShUR3kKP3AI'
+
+
+bot = telebot.TeleBot(TOKEN)
+@bot.message_handler(commands=['start'])
+def start(message):
+    
+    bot.send_message(message.chat.id, 'Привет, сейчас вышлю последний пост!')
+    last_post_text = ''
+    videos_for_delete = []
+    while True:
+        if is_new_post_piblished(last_post_text):
+            text, photos_list, videos = main()
+            medias = []
+            if photos_list:
+                for i in photos_list:
+                    medias.append(types.InputMediaPhoto(i))
+                bot.send_media_group(message.chat.id, medias)
+        
+            bot.send_message(message.chat.id,text)
+        
+            if videos:
+                for i in videos:
+                    video = open(i, 'rb')
+                    bot.send_video(message.chat.id, video, supports_streaming=True)
+                
+            last_post_text = text
+            videos_for_delete.extend(videos)
+            time.sleep(3600)
+        
+        else:
+            if videos_for_delete and len(videos_for_delete)>1:
+                delete_videos(videos_for_delete[:-1])
+                videos_for_delete = videos_for_delete[-1:]
+            time.sleep(3600)
+
+            
+
+bot.polling(non_stop=True)
---
+++ parse_and_delete_f.py
@@ -0,0 +1,92 @@
+import requests
+import os
+import youtube_dl
+
+
+def main():
+    TOKEN = '75068529750685297506852958757c7f22775067506852914ebcb3cdcaa45f57558dc20'
+
+    VERSION = 5.131
+    DOMAIN = 'biobezpredel'
+
+    link_1 = 'https://vk.com/biobezpredel?z=photo-76771597_'
+    link_2 = '%2Falbum-76771597_00%2Frev'
+
+    def give_phtVd_links(attach: dict):
+        res = []
+        res_vid = []
+        if len(attach) > 1:
+            for i in range(len(attach)):
+                if attach[i]['type'] == 'photo':
+                    res.append(link_1 + str(attach[i]['photo']['id']) + link_2)
+                if attach[i]['type'] == 'video':
+                    print()
+                    lnk = 'vk.com/video' + \
+                        str(attach[i]['video']['owner_id']) + \
+                        '_' + str(attach[i]['video']['id'])
+                    res_vid.append(lnk)
+
+        else:
+            if attach[0]['type'] == 'photo':
+                res.append(link_1 + str(attach[0]['photo']['id']) + link_2)
+            elif attach[0]['type'] == 'video':
+                lnk = 'vk.com/video' + \
+                    str(attach[0]['video']['owner_id']) + \
+                    '_' + str(attach[0]['video']['id'])
+                res_vid.append(lnk)
+
+        return res, res_vid
+
+    responce = requests.get('https://api.vk.com/method/wall.get',
+                            params={
+                                'access_token': TOKEN,
+                                'v': VERSION,
+                                'domain': DOMAIN,
+                                'count': 2,
+                            })
+
+    data = responce.json()['response']['items']
+
+    text = data[1]['text']
+    photo_links, video_links = give_phtVd_links(data[1]['attachments'])
+    video_names = []
+    if video_links:
+        ydl_opts = {}
+        for link in video_links:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([link])
+            video_names.append(
+                f'Video by BioBeZpredel [Биохакинг _ Спорт _ Дизмораль]-{link[-19:]}.mp4')
+
+    return (text, photo_links, video_names)
+
+
+def is_new_post_piblished(last_post):
+    TOKEN = '75068529750685297506852958757c7f22775067506852914ebcb3cdcaa45f57558dc20'
+
+    VERSION = 5.131
+    DOMAIN = 'biobezpredel'
+
+    responce = requests.get('https://api.vk.com/method/wall.get',
+                            params={
+                                'access_token': TOKEN,
+                                'v': VERSION,
+                                'domain': DOMAIN,
+                                'count': 2,
+                            })
+
+    data = responce.json()['response']['items']
+
+    text = data[1]['text']
+    if text == last_post:
+        return False
+    return True
+
+def delete_videos(video_list: list):
+    if video_list:
+        for video in video_list:
+            os.remove(video)
+
+if __name__ == '__main__':
+    main()
+
