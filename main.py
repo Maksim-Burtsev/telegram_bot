dc839c14fc43370702c71a13ad2f9f123fdaaac7
Maksim Burtsev <zadrot-lol@list.ru>
2022-01-29 13:13:39 +0300

    V2.0

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import telebot
 from telebot import types
 import time
 import requests
-from parse_and_delete_f import main, is_new_post_piblished,delete_videos
+from parse_and_delete_f import main, is_new_post_piblished, delete_videos
 
 TOKEN = '5291208463:AAEokJAK6ISX7TiwJ4pBYZKoShUR3kKP3AI'
 
 
 bot = telebot.TeleBot(TOKEN)
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    
+
     bot.send_message(message.chat.id, 'Привет, сейчас вышлю последний пост!')
     last_post_text = ''
     videos_for_delete = []
@@ -18,28 +20,29 @@ def start(message):
         if is_new_post_piblished(last_post_text):
             text, photos_list, videos = main()
             medias = []
+
+            bot.send_message(message.chat.id, text)
+            
             if photos_list:
                 for i in photos_list:
                     medias.append(types.InputMediaPhoto(i))
                 bot.send_media_group(message.chat.id, medias)
-        
-            bot.send_message(message.chat.id,text)
-        
+
             if videos:
                 for i in videos:
                     video = open(i, 'rb')
-                    bot.send_video(message.chat.id, video, supports_streaming=True)
-                
+                    bot.send_video(message.chat.id, video,
+                                   supports_streaming=True)
+
             last_post_text = text
             videos_for_delete.extend(videos)
             time.sleep(3600)
-        
+
         else:
-            if videos_for_delete and len(videos_for_delete)>1:
+            if videos_for_delete and len(videos_for_delete) > 1:
                 delete_videos(videos_for_delete[:-1])
                 videos_for_delete = videos_for_delete[-1:]
             time.sleep(3600)
 
-            
 
 bot.polling(non_stop=True)
